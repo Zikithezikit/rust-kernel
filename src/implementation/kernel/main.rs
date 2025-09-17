29e11dd419f52891f59aeaadaf0464085f1591a8
Zikithezikit <zikithezikit@gmail.com>
2025-09-17 22:53:04 +0300

    Added keyboard Us-104 native support, just prints the pressed keys on the screen

--- src/implementation/kernel/main.rs
+++ src/implementation/kernel/main.rs
@@ -6,6 +6,8 @@ mod panic;
 mod std_lib;
 mod interrupts;
 
+use x86_64::instructions::interrupts as x86_64_interrupts;
+
 
 #[no_mangle] // prevents Rust from mangling the name
 pub extern "C" fn kernel_main() -> ! {
@@ -13,5 +15,11 @@ pub extern "C" fn kernel_main() -> ! {
     std_lib::vga::clear_screen();
     std_lib::vga::println("Hello, World!");
     std_lib::vga::println("This is my kernel.");
+
+    unsafe { interrupts::init_pic(); }
+    interrupts::init_idt();
+    x86_64_interrupts::enable();
+
+    loop{ x86_64::instructions::hlt(); };
 }
 
