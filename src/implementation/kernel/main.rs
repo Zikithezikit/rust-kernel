df2bd52541ed7e5075d3c28ddfae219d9737ee67
Ziki <80419617+Zikithezikit@users.noreply.github.com>
2025-09-17 22:55:13 +0300

    Revert "Interupts"

--- src/implementation/kernel/main.rs
+++ src/implementation/kernel/main.rs
@@ -1,12 +1,8 @@
 #![no_std]
 #![no_main]
-#![feature(abi_x86_interrupt)]
 
 mod panic;
 mod std_lib;
-mod interrupts;
-
-use x86_64::instructions::interrupts as x86_64_interrupts;
 
 
 #[no_mangle] // prevents Rust from mangling the name
@@ -15,11 +11,6 @@ pub extern "C" fn kernel_main() -> ! {
     std_lib::vga::clear_screen();
     std_lib::vga::println("Hello, World!");
     std_lib::vga::println("This is my kernel.");
-
-    unsafe { interrupts::init_pic(); }
-    interrupts::init_idt();
-    x86_64_interrupts::enable();
-
-    loop{ x86_64::instructions::hlt(); };
+    loop {}
 }
 
