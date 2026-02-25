0e4cc37a1322410d63384e606e720d393480b3df
Zikithezikit <zikithezikit@gmail.com>
2026-02-25 16:40:42 +0200

    Working booting kernel

--- src/implementation/kernel/interrupts.rs
+++ src/implementation/kernel/interrupts.rs
@@ -1,15 +1,14 @@
 #![allow(dead_code)]
 
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use spin::Once;
-use spin::Mutex;
-use x86_64::instructions::port::Port;
-use pc_keyboard::{Keyboard, layouts::Us104Key, ScancodeSet1, HandleControl};
 use crate::std_lib::vga;
+use pc_keyboard::{layouts::Us104Key, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
+use spin::Once;
+use x86_64::instructions::port::Port;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 static IDT: Once<InterruptDescriptorTable> = Once::new();
 static KEYBOARD: Mutex<Option<Keyboard<Us104Key, ScancodeSet1>>> = Mutex::new(None);
-
 
 const MASTER_CONTROL: u16 = 0x20;
 const MASTER_DATA: u16 = 0x21;
@@ -17,9 +16,8 @@ const MASTER_DATA: u16 = 0x21;
 const SLAVE_CONTROL: u16 = 0xA0;
 const SLAVE_DATA: u16 = 0xA1;
 
-
 #[derive(Clone, Copy)]
-pub enum IrqNumber  {
+pub enum IrqNumber {
     SystemTimer,
     Keyboard,
     InterruptController,
@@ -36,8 +34,7 @@ pub enum IrqNumber  {
     FPU,
     PrimaryATA,
     SecondaryATA,
-} 
-
+}
 
 pub fn init_idt() {
     let mut idt = InterruptDescriptorTable::new();
@@ -46,42 +43,18 @@ pub fn init_idt() {
     idt[33].set_handler_fn(keyboard_interrupt_handler);
     IDT.call_once(|| idt).load();
 
-    *KEYBOARD.lock() = Some(Keyboard::new(ScancodeSet1::default(), Us104Key, HandleControl::Ignore));
-}
-
-pub unsafe fn init_pic() {
-    let mut master = Port::new(0x20u16);
-    let mut master_data = Port::new(0x21u16);
-    let mut slave = Port::new(0xA0u16);
-    let mut slave_data = Port::new(0xA1u16);
-
-    // Start initialization (ICW1)
-    master.write(0x11u8);
-    slave.write(0x11u8);
-
-    // Set vector offsets (ICW2)
-    master_data.write(0x20u8); // Master offset
-    slave_data.write(0x28u8);  // Slave offset
-
-    // Tell master about slave (ICW3)
-    master_data.write(0x04u8); // IRQ2 has slave
-    slave_data.write(0x02u8);  // Slave identity
-
-    // ICW4: 8086 mode
-    master_data.write(0x01u8);
-    slave_data.write(0x01u8);
-
-    // Mask all IRQs (just in case)
-    master_data.write(0x0u8);
-    slave_data.write(0x0u8);
-    
+    *KEYBOARD.lock() = Some(Keyboard::new(
+        ScancodeSet1::default(),
+        Us104Key,
+        HandleControl::Ignore,
+    ));
 }
 
 pub unsafe fn init_pic() {
     let mut master_control = Port::new(MASTER_CONTROL);
     let mut master_data = Port::new(MASTER_DATA);
     let mut slave_control = Port::new(SLAVE_CONTROL);
-    let mut slave_data = Port::new(SLAVE_CONTROL);
+    let mut slave_data = Port::new(SLAVE_DATA);
 
     // Start initialization (ICW1)
     master_control.write(0x11u8);
@@ -89,11 +62,11 @@ pub unsafe fn init_pic() {
 
     // Set vector offsets (ICW2)
     master_data.write(MASTER_CONTROL as u8); // Master offset
-    slave_data.write(SLAVE_CONTROL as u8);  // Slave offset
+    slave_data.write(SLAVE_CONTROL as u8); // Slave offset
 
     // Tell master about slave (ICW3)
     master_data.write(0x04u8); // IRQ2 has slave
-    slave_data.write(0x02u8);  // Slave identity
+    slave_data.write(0x02u8); // Slave identity
 
     // ICW4: 8086 mode
     master_data.write(0x01u8);
@@ -102,12 +75,13 @@ pub unsafe fn init_pic() {
     // Mask all IRQs (just in case)
     master_data.write(0x0u8);
     slave_data.write(0x0u8);
-    
 }
 
 extern "x86-interrupt" fn debug_handler(_stack_frame: InterruptStackFrame) {
     vga::println("EXCEPTION: DEBUG");
-    loop { x86_64::instructions::hlt(); }
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
@@ -125,8 +99,10 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
                 if let Ok(Some(_event)) = keyboard.add_byte(scancode) {
                     if let Some(key) = keyboard.process_keyevent(_event) {
                         match key {
-                            pc_keyboard::DecodedKey::Unicode(character) => vga::print_char(character as u8),
-                            pc_keyboard::DecodedKey::RawKey(_key) => {} 
+                            pc_keyboard::DecodedKey::Unicode(character) => {
+                                vga::print_char(character as u8)
+                            }
+                            pc_keyboard::DecodedKey::RawKey(_key) => {}
                         }
                     }
                 }
@@ -136,12 +112,13 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     }
 }
 
-
 fn pic_end_of_interrupt(irq: u8) {
     let mut master = Port::new(MASTER_CONTROL);
     let mut slave = Port::new(SLAVE_CONTROL);
     const END_OF_INTERRUPT: u8 = 0x20;
 
-    if irq >= 8 { unsafe { slave.write(END_OF_INTERRUPT) }; }
+    if irq >= 8 {
+        unsafe { slave.write(END_OF_INTERRUPT) };
+    }
     unsafe { master.write(END_OF_INTERRUPT) };
 }
